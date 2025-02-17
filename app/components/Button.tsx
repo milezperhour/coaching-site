2c40856e2e57c70f4116f4b2b4bedbb4a77d4406
Miles Bretall <milesbretall@Miless-MBP-2.askey.com>
2025-02-16 17:20:04 -0800

    changed button color

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -8,7 +8,7 @@ const Button = () => {
   return (
     <div className="mt-8 mb-4 flex justify-center items-center">
       <Link href="/book">
-        <button className="bg-orange-500 text-white font-bold px-8 py-4 rounded hover:bg-orange-400 transition uppercase w-80 text-2xl">
+        <button className="bg-emerald-500 text-white font-bold px-8 py-4 rounded hover:bg-emerald-400 transition uppercase w-80 text-2xl">
             Book A Call
         </button>
       </Link>
